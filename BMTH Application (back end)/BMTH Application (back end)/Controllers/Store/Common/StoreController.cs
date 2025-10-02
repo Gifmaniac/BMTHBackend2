09fa3c98dddd7e6b8c1f74ee54fd82d70813c71e
Gifmaniac <w.vd.b@live.nl>
2025-10-02 09:44:12 +0200

    Store controller

--- BMTH Application (back end)/BMTH Application (back end)/Controllers/Store/Common/StoreController.cs
+++ BMTH Application (back end)/BMTH Application (back end)/Controllers/Store/Common/StoreController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using APIContracts.DTOs.StoreItems.Shirts;
 using Contracts.Enums.Store;
+using BMTH_Application__back_end_.Mappers.StoreItems.TShirts;
+using BusinessLayer.Services;
 
 
 namespace BMTH_Application__back_end_.Controllers.Store.Common
@@ -13,8 +15,33 @@ namespace BMTH_Application__back_end_.Controllers.Store.Common
     public class StoreController : ControllerBase
     {
         [HttpGet]
-        public IActionResult GetStoreCategories([FromQuery] string? gender)
+        public IActionResult GetStoreCategories([FromQuery] string? Category)
         {
+            // Parses the genders.
+            Genders? parsedCategory = null;
+
+            // Convert the string to Enum, ignores uppercases if success filters on gender
+            if (!string.IsNullOrWhiteSpace(Category) && Enum.TryParse<Genders>(Category, true, out var category))
+            {
+                parsedCategory = category;
+            }
+
+            // Makes a invalid check. 
+            else
+            {
+                return NotFound();
+            }
+
+
+            // Get the filtered entities
+            // var entities = _tShirtService.GetShirtsByGender(parsedGender);
+
+            // Maps the entities
+            // var overview = entities.Select(TShirtApiMapper.ToOverviewDto);
+
+            // return Ok(overview);
+
+
             var categories = new List<StoreCategoryDto>
             {
                 new StoreCategoryDto { Category = StoreCategoryType.TShirts, DisplayName = "T-Shirts" },
